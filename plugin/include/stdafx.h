d6b11c4cf4ab2db158b65f27fef20797dd186e86
Victor Istomin <victor.end.mail.here@gmail.com>
2014-08-06 02:10:02 +0300

    Created VS solution with debug x32 build

--- plugin/include/stdafx.h
+++ plugin/include/stdafx.h
@@ -10,7 +10,7 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
-#include <CRT/crt.hpp>
+//#include <CRT/crt.hpp>
 #include <plugin.hpp>
 #include <initguid.h>
 
